7e1e7c061f3cbc99b6e07684c06980524b200430
匂坂 勇仁 <sagisakahayato@niozaka-no-MacBook-Air.local>
2015-01-16 15:24:41 +0900

    no message

--- Bodies.py
+++ Bodies.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys ##sysをインポートしないと12行目のsys.exit()が成り立たない
+import sys
 
-##from jp.ac.kyoto_su.cse.pl.Bodies.Example import Example as example ##from メインまでのファイルまでの道のり import Exampleファイル
-import jp.ac.kyoto_su.cse.pl.Bodies.Example ##総理大臣の書き方で
-
+from jp.ac.kyoto_su.cse.sh.Bodies.Example import Example as example
 
 if __name__ == '__main__':
-	an_example =jp.ac.kyoto_su.cse.pl.Bodies.Example.Example()##importまでの道のり.クラス呼び出し
-	sys.exit(an_example.main())##sys.exit([arg]) pythonの終了
+	sys.exit(example.main())
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup ##変更なし
+from distutils.core import setup
 
 setup(
-	name='Bodies',##パッケージの名前を参照
-	version='1.0.0',##アプリケーションのバージョン
-	author='AOKI Atsushi',##パッケージの作者
-	author_email='atsushi@cc.kyoto-su.ac.jp',##パッケージ作者の電子メールアドレス
-	url='http://www.cc.kyoto-su.ac.jp/~atsushi/',##パッケージのホームページ
-	description='Bodies written by Python and PyOpenGL',##パッケージについての簡単な概要説明
-	long_description='ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを3次元描画する',##パッケージについての詳細な説明
-	  ##license='GNU Affero General Public License',##パッケージのライセンス
-	platforms='OS X (10.10.1)',##プラットフォームのリスト
-	  ##packages=['jp'],##参照ファイル
+	name='Bodies',
+	version='1.0.0',
+	author='AOKI Atsushi',
+	author_email='atsushi@cc.kyoto-su.ac.jp',
+	url='http://www.cc.kyoto-su.ac.jp/~atsushi/',
+	description='Bodies written by Python and PyOpenGL',
+	long_description='ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを3次元描画する',
+	license='The BSD 2-Clause License',
+	platforms='OS X (10.10.1)',
+	packages=['jp'],
 )
